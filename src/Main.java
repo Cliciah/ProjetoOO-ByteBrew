fcdce3179e546824a4557ff7a34d8977456d9aca
lianeiv <julianeiv4@gmail.com>
2026-06-27 04:48:04 -0300

    estrutura inicial dos produtos

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,27 @@ public class Main {
         System.out.println("Iniciando o sistema Byte & Brew...");
     }
 }
+
+// cardapio
+public abstract class Produto {
+
+
+    private int codigo;
+    private double precoBase;
+    private String nome;
+    private int quantidadeEstoque;
+
+    public Produto(int codigo, String nome, double precoBase, int quantidadeEstoque) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.precoBase = precoBase;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void atualizarEstoque() {
+    }
+
+    public int consultarEstoque() {
+        return this.quantidadeEstoque;
+    }
+}
