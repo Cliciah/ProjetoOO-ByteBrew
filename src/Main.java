47dad6c02e55b8fbf08d23b6ae666fef71048950
lianeiv <julianeiv4@gmail.com>
2026-07-02 14:53:55 -0300

    corrigi bugs da parte de produtos

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
-public class Main {
+package br.edu.cafeteria.modelo;
+
+class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando o sistema Byte & Brew...");
     }
 }
 
-
-package br.edu.cafeteria.modelo;
-
+// produtos
 public abstract class Produto {
     protected int codigo;
     protected double precoBase;
     protected String nome;
     protected int quantidadeEstoque;
-} 
 
     public Produto(int codigo, String nome, double precoBase, int quantidadeEstoque) {
         this.codigo = codigo;
@@ -25,6 +24,9 @@ public abstract class Produto {
         this.quantidadeEstoque += quantidade;
     }
 
+    public void atualizarEstoque() {
+    }
+
     public int consultarEstoque() {
         return this.quantidadeEstoque;
     }
@@ -32,32 +34,23 @@ public abstract class Produto {
     public int getCodigo() { return codigo; }
     public double getPrecoBase() { return precoBase; }
     public String getNome() { return nome; }
-}
-// produtos
-abstract class Produto {
-
-    private int codigo;
-    private double precoBase;
-    private String nome;
-    private int quantidadeEstoque;
-
-    public Produto(int codigo, String nome, double precoBase, int quantidadeEstoque) {
-        this.codigo = codigo;
-        this.nome = nome;
-        this.precoBase = precoBase;
-        this.quantidadeEstoque = quantidadeEstoque;
-    }
-
-    public void atualizarEstoque() {
-    }
-
-    public int consultarEstoque() {
-        return this.quantidadeEstoque;
-    }
+    
     public abstract String getTipo(); 
 }
 
 class Bebida extends Produto { 
+    public Bebida(int codigo, String nome, double precoBase, int quantidadeEstoque) {
+        super(codigo, nome, precoBase, quantidadeEstoque);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Bebida";
+    }
+}
+
+//bebida
+
     
     // atributos necessários para puxar os dados de Produto
     public Bebida(int codigo, String nome, double precoBase, int quantidadeEstoque) {
