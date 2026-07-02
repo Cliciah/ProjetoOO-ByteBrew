c7cab350e327442ae37143a326bbdcdf90ef755a
lianeiv <julianeiv4@gmail.com>
2026-07-02 14:45:32 -0300

    atributos para encapsulamento

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,18 @@ public class Main {
     }
 }
 
-// produtos
-public abstract class Produto {
 
+package br.edu.cafeteria.modelo;
+
+public abstract class Produto {
+    protected int codigo;
+    protected double precoBase;
+    protected String nome;
+    protected int quantidadeEstoque;
+} 
+
+// produtos
+abstract class Produto {
 
     private int codigo;
     private double precoBase;
@@ -26,21 +35,27 @@ public abstract class Produto {
     public int consultarEstoque() {
         return this.quantidadeEstoque;
     }
+    public abstract String getTipo(); 
 }
 
-//atributo para a Interface de produtos
-public abstract class Produto {
-    public abstract String getTipo();
-}
+class Bebida extends Produto { 
+    
+    // atributos necessários para puxar os dados de Produto
+    public Bebida(int codigo, String nome, double precoBase, int quantidadeEstoque) {
+        super(codigo, nome, precoBase, quantidadeEstoque);
+    }
 
-public class Bebida extends Produto {
     @Override
     public String getTipo() {
         return "Bebida";
     }
 }
 
-public class Comida extends Produto {
+class Comida extends Produto { 
+    public Comida(int codigo, String nome, double precoBase, int quantidadeEstoque) {
+        super(codigo, nome, precoBase, quantidadeEstoque);
+    }
+
     @Override
     public String getTipo() {
         return "Comida";
