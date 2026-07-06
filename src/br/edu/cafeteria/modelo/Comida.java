e59043b90daac243fb1c2b90ebb89ce963434fcf
lianeiv <julianeiv4@gmail.com>
2026-07-05 22:59:26 -0300

    correções no codigo

--- src/br/edu/cafeteria/modelo/Comida.java
+++ src/br/edu/cafeteria/modelo/Comida.java
@@ -2,26 +2,41 @@ package br.edu.cafeteria.modelo;
 
 public class Comida extends Produto { 
     private int tempoPreparo;
-    private boolean isVeganoSemGluten;
+    private boolean isVegano;
+    private boolean isSemGluten;
 
     public Comida(String codigo, String nome, double precoBase, int quantidadeEstoque) {
         super(codigo, nome, precoBase, quantidadeEstoque);
     }
 
-    public Comida(String codigo, String nome, double precoBase, int quantidadeEstoque, int tempoPreparo, boolean isVeganoSemGluten) {
+    public Comida(String codigo, String nome, double precoBase, int quantidadeEstoque, int tempoPreparo, boolean isVegano, boolean isSemGluten) {
         super(codigo, nome, precoBase, quantidadeEstoque); 
         this.tempoPreparo = tempoPreparo;
-        this.isVeganoSemGluten = isVeganoSemGluten;
+        this.isVegano = isVegano;
+        this.isSemGluten = isSemGluten;
     }
 
     public void alterarTempoPreparo(int novoTempo) {
         this.tempoPreparo = novoTempo;
     }
 
-    public void definirVeganoSemGluten(boolean ePreferencia) {
-        this.isVeganoSemGluten = ePreferencia;
+    public void definirVegano(boolean vegano) {
+        this.isVegano = vegano;
     }
 
-    public int getTempoPreparo() { return tempoPreparo; }
-    public boolean isVeganoSemGluten() { return isVeganoSemGluten; }
+    public void definirSemGluten(boolean semGluten) {
+        this.isSemGluten = semGluten;
+    }
+
+    public int getTempoPreparo() { 
+        return tempoPreparo; 
+    }
+    
+    public boolean isVegano() { 
+        return isVegano; 
+    }
+    
+    public boolean isSemGluten() { 
+        return isSemGluten; 
+    }
 }
